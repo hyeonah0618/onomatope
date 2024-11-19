0328ac132dc1b61beaab2b94911f58f7f77e26fd
hyeonah0618 <hyeonah0618@gmail.com>
2024-11-20 02:06:22 +0900

    scene3 추가

--- js/script3.js
+++ js/script3.js
@@ -1,5 +1,27 @@
 // GSAP 초기화
 gsap.registerPlugin(ScrollTrigger);
+// ... existing code ...
+function openCenteredWindow(url) {
+    const width = 600;
+    const height = 600;
+    
+    // 화면 중앙 좌표 계산
+    const left = (window.screen.width - width) / 2;
+    const top = (window.screen.height - height) / 2;
+    
+    // 팝업창 열기
+    window.open(url, '_blank', 
+        `width=${width},
+         height=${height},
+         left=${left},
+         top=${top},
+         menubar=no,
+         toolbar=no,
+         location=no,
+         status=no,
+         scrollbars=yes`
+    );
+}
 
 // 전체 슬라이드 애니메이션
 gsap.to(".slides", {
@@ -10,7 +32,7 @@ gsap.to(".slides", {
         start: "top top",
         end: () => `+=${document.querySelector(".slides").scrollWidth - window.innerWidth}`,
         pin: true,
-        scrub: 1,
+        scrub: 3,
         invalidateOnRefresh: true,
         markers: true
     }
@@ -23,8 +45,18 @@ gsap.to("#running-person", {
         trigger: ".scene1",
         start: "top top",
         end: "+=4000",
-        scrub: 1
+        scrub: 3
     }
 });
-
-// 다른 요소들의 애니메이션도 필요에 따라 추가
+gsap.fromTo("#bird", {
+    x: 0
+    },
+    {
+    x: -1000, y: 100,
+    scrollTrigger: {
+        trigger: ".scene1",
+        start: "top top",
+        end: "+=2000",
+        scrub: 3
+    }
+});
